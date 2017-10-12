8a36c175764c085604ecf44b590b982e90cee623
Unknown <up703451@myport.ac.uk>
2017-10-12 12:50:56 +0100

    Start
    Colour scheme needs thinking about 
    Navigation sort of done need to do the drop downs which starting now
    Need pictures for the carosel and what text to put with it if any
    Card layout for the rest of data and some minor chanegs needed in width and height depending on what everyone thinks 
    
    layout css file has the columns, media query for different screen sizes and the card layout
    
    navigation holds all the nav css stuff 
    
    Index is the home page css

---
+++ js/navigation.js
@@ -0,0 +1,25 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.className === "navigation") {
+        x.className += " responsive";
+        $(".searchBox").show();
+    } else {
+        x.className = "navigation";
+        $(".searchBox").hide();
+    }
+}
+
+$(window).resize(function() {
+
+   if($(window).width() > 600) {
+       $(".searchBox").show();
+   }
+
+});
+
+<!--Materializecss Slider-->
+$(document).ready(function () {
+    // Plugin initialization
+    $('.slider').slider();
+})
